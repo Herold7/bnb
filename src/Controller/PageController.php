96e87803b49b0b20610f8b8e59110da745aad7c8
Herold7 <makpier@gmail.com>
2024-01-21 12:28:06 +0100

    💄 Mise à jour du style

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -14,4 +14,13 @@ class PageController extends AbstractController
         return $this->render('page/index.html.twig', [
         ]);
     }
+
+        // Test route for designing the confirmation
+    #[Route('/email', name: 'email')]
+    public function email(): Response
+    {
+        return $this->render('registration/confirmation_email.html.twig', [
+            'signedUrl' => 'https://example.com/signed-url',
+        ]);
+    }
 }
